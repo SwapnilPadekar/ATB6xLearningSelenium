c702dd1c794c9930030a143c380a9b4b25ab932e
SwapnilPadekar <slpadekar29@gmail.com>
2024-07-15 19:47:15 +0530

    11th July code entered

--- src/test/java/com/thetestingacademy/HelloWorld.java
+++ src/test/java/com/thetestingacademy/HelloWorld.java
@@ -5,9 +5,8 @@ import org.openqa.selenium.edge.EdgeDriver;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        EdgeDriver driver = new EdgeDriver(); //POST request to create a new session
+        EdgeDriver driver = new EdgeDriver();
         driver.get("https://courses.thetestingacademy.com/");
-        //driver.get - API POST request to navigate to URL
-        //Every statement in the script will be GET/POST/PUT/PATCH request to the driver run on the browsers
+
     }
 }
